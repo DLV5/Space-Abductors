4bd146a4c8d1727bd2d3cec6ba09b2d72d55ea74
asorokin111 <Artem.Sorokin@vuoksi.fi>
2023-05-29 13:17:13 +0300

    Prevent the player from going off-screen

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,10 +6,28 @@ public class Movement : MonoBehaviour
     [SerializeField]
     private float speed = 5.0f;
     private Vector2 _direction;
+    private Vector2 _minScreenBounds;
+    private Vector2 _maxScreenBounds;
+
+    private void Awake()
+    {
+        _minScreenBounds = Camera.main.ScreenToWorldPoint(Vector2.zero);
+        _maxScreenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    }
 
     private void Update()
     {
         _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         transform.position += (Vector3)_direction * speed * Time.deltaTime;
+
+        // Most sane unity code
+        if (transform.position.x > _maxScreenBounds.x)
+            transform.position = new Vector2(_maxScreenBounds.x, transform.position.y);
+        if (transform.position.y > _maxScreenBounds.y)
+            transform.position = new Vector2(transform.position.x, _maxScreenBounds.y);
+        if (transform.position.x < _minScreenBounds.x)
+            transform.position = new Vector2(_minScreenBounds.x, transform.position.y);
+        if (transform.position.y < _minScreenBounds.y)
+            transform.position = new Vector2(transform.position.x, _minScreenBounds.y);
     }
 }
