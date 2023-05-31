0dbe49eb93aeb0782baa3bb5f4ca204e8390ee3e
asorokin111 <Artem.Sorokin@vuoksi.fi>
2023-05-31 09:57:25 +0300

    Revert "The player's weapon now inherits an Attacker class"
    This reverts commit 100247dec83c231bc9f49893993874b40933eb31.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -2,15 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : Attacker
+public class Weapon : MonoBehaviour
 {
+    public int damage;
+    public int cooldown;
+    public float spreadAngle;
+
     [SerializeField]
     private Texture2D _crosshair;
 
+    private static GameObjectsPool gameObjectsPool;
+    private bool _canShoot = true;
 
     private void Awake()
     {
         Cursor.SetCursor(_crosshair, new Vector2(_crosshair.width / 2, _crosshair.height / 2), CursorMode.Auto);
+    }
+
+    private void Start()
+    {
+        if (gameObjectsPool == null)
+            gameObjectsPool = GameManager.Instance.GetGameObjectsPool("PlayerBullet");
     }
 
     private void Update()
@@ -18,6 +30,32 @@ public class Weapon : Attacker
         if (Input.GetKey(KeyCode.Mouse0) && _canShoot)
         {
             Shoot();
+            _canShoot = false;
+            StartCoroutine(EnterCooldown());
         }
+    }
+
+    public void Shoot()
+    {
+        foreach (var obj in gameObjectsPool.pool)
+        {
+            if (!obj.activeSelf)
+            {
+           
+                obj.SetActive(true);
+                obj.transform.position = transform.position;
+                Quaternion spreadRotation = Quaternion.Euler(0f, 0f, Random.Range(-spreadAngle / 2, spreadAngle / 2));
+                var target = (spreadRotation * (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+                obj.GetComponent<Bullet>().direction = target.normalized;
+
+                break;
+            }
+        }
+    }
+
+    private IEnumerator EnterCooldown()
+    {
+        yield return new WaitForSeconds(cooldown);
+        _canShoot = true;
     }
 }
