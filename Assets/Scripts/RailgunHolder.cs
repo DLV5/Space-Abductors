c210430d40deda5be4cf8f53dc5b89d53a1cae02
asorokin111 <Artem.Sorokin@vuoksi.fi>
2023-06-14 10:15:09 +0300

    Fixed rotating railgun

---
+++ Assets/Scripts/RailgunHolder.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailgunHolder : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector2 mousePos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 dir = mousePos - (Vector2)transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = rotation;
+    }
+}
