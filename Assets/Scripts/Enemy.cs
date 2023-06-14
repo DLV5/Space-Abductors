87b9119b56aa5c3c15f75953e7221c8cd96544f6
Dmitry <diman567tak@gmail.com>
2023-06-14 12:54:22 +0300

    AddedShaderAndBoss

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,10 @@ public class Enemy : Attacker, IDamageable
     protected int health;
     public int Health { get => health; set => health = value; }
 
+    [SerializeField] protected float _flashTime = 0.25f;
+
+    private SpriteRenderer _spriteRenderer;
+    private Material _material;
 
     protected EnemyStates currentState = EnemyStates.FlyingToTheScreen;
     protected enum EnemyStates
@@ -17,13 +21,28 @@ public class Enemy : Attacker, IDamageable
         Leaving
     }
 
+    protected void Awake()
+    {
+        Init();
+    }
+
+    protected void Init()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _material = _spriteRenderer.material;
+    }
     public void Damage(int damage)
     {
         Health -= damage;
         if (Health <= 0) 
         {
+            //StopCoroutine(DamageFlasher());
             gameObject.SetActive(false);
         }
+        if(gameObject.activeSelf) 
+                CallDamageFlash();
+            _material.SetFloat("_FlashAmount", 0);
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -35,4 +54,25 @@ public class Enemy : Attacker, IDamageable
             collision.gameObject.SetActive(false);
         }
     }
+
+    public void CallDamageFlash()
+    {
+        StartCoroutine(DamageFlasher());
+    }
+    protected IEnumerator DamageFlasher()
+    {
+        if (_material == null || _spriteRenderer == null) Init();
+        float currentFlashAmount = 0f;
+        float elapsedTime = 0f;
+        while (elapsedTime < _flashTime)
+        {
+            elapsedTime += Time.deltaTime;
+
+            currentFlashAmount = Mathf.Lerp(1f, 0f, (elapsedTime / _flashTime));
+            _material.SetFloat("_FlashAmount", currentFlashAmount);
+
+            yield return null; 
+        }
+    }
+    
 }
